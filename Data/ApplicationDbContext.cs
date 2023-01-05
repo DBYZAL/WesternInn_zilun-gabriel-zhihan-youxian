c9c9844158b13aeb4328953cfc64fe424015e906
DBYZAL <121598624+DBYZAL@users.noreply.github.com>
2023-01-05 20:37:29 +1100

    group project

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WesternInn_zilun_gabriel_zhihan_youxian.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
